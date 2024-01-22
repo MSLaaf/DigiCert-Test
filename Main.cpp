afa3feb65d5325f3bd35b05f6fa7e6aab7787aac
Maurice Smulders <Maurice.Smulders@gmail.com>
2024-01-22 03:38:44 +0000

    Sebug info

--- Main.cpp
+++ Main.cpp
@@ -1,41 +1,71 @@
 #include "docopt/docopt.h"
-
+ // https://github.com/docopt/docopt.cpp/tree/master
 #include <iostream>
+
+#include <cstdlib>
+
 #include <map>
+
 #include "Book.hpp"
 
 static const char USAGE[] =
 R"(C.R.U.D. Library Book Tool
 
    Usage:
-     CRUD_Book list  same as 'CRUD_Book read' without parameters
-     CRUD_Book create title=<title> author=<author> [isbn=<ISBN>] [year=<year>] [loaned=Yes|No]
-     CRUD_Book read [id=<id>] [title=<title>] [author=<author>] [isbn=<ISBN>] [year=<year>] [loaned=Yes|No]
-     CRUD_Book update id=<id> [title=<title>] [author=<author>] [isbn=<ISBN>] [year=<year>] [loaned=yes|no]
-     CRUD_Book delete (id=<id>|title=<title|isbn=<ISBN>|all)
+     CRUD_Book list
+     CRUD_Book create --title=<title> --author=<author> [--isbn=<isbn>] [--year=<year>] [--loaned]
+     CRUD_Book read [--id=<id>] [--title=<title>] [--author=<author>] [--isbn=<isbn>] [--year=<year] [--loaned]
+     CRUD_Book update [--id=<id>] [--ti--tle=<title>] [--author=<author>] [--isbn=<isbn>] [--year=<year>] [(--loaned|--returned)]
+     CRUD_Book delete (--id=<id>|--title=<title>|--isbn=<isbn>|--all)
      CRUD_Book ( -h | --help )
-     CRUD_Book --version
-     NOTE: You need to quote strings with spaces
+     CRUD_Book ( -v | --version )
 
    Options:
      -h --help
-     --version
-     --id <id>
-     --title <title>  Use \" \' to make sure spaces are included
-     --author <author"  
+     -v --version
+     --id=<id>          RecordID
+     --title=<title>    Quote to make sure spaces are included
+     --author=<author>  Quote Author Name
+     --isbn=<isbn>      ISBN
+     --year=<year>      year of release
+     --loaned
+     --returned
+     --debug            debug output
 )";
 
-int main(int argc, const char** argv)
-{
-    std::map<std::string, docopt::value> args
-        = docopt::docopt(USAGE,
-                         { argv + 1, argv + argc },
-                         true,               // show help if requested
-                         "Naval Fate 2.0");  // version string
+bool debugFlag = false;
 
-    for(auto const& arg : args) {
-        std::cout << arg.first <<  arg.second << std::endl;
-    }
+int main(int argc,
+   const char ** argv) {
+   std::map < std::string, docopt::value > args = docopt::docopt(USAGE, {
+         argv + 1,
+         argv + argc
+      },
+      true, // show help if requested
+      "CRUD_Book 1.0"); // version stri
+   debugFlag = std::getenv("DEBUG_CRUD") != NULL;
 
-    return 0;
+   if (debugFlag) {
+      std::cout << "Argument List:" << std::endl;
+      for (auto
+         const & arg: args) {
+         std::cout << "\"" << arg.first << "\"=\"" << arg.second << "\"" << std::endl;
+      }
+   }
+
+   if (args["list"]) {
+      std::cout << "List All records" << std::endl;
+   } else if (args["create"]) {
+      std::cout << "Create" << std::endl;
+   } else if (args["delete"]) {
+      std::cout << "Delete" << std::endl;
+   } else if (args["read"]) {
+      std::cout << "Read" << std::endl;
+   } else if (args["update"]) {
+      std::cout << "Update" << std::endl;
+   } else {
+      std::cout << "Invalid command" << std::endl;
+      return 1;
+   }
+   return 0;
 }
